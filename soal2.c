b1d2adf1a06ad175e6ea03bb45d1b9baa7b859db
Rafi Fathia <rafifathia@gmail.com>
2026-05-04 08:59:08 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
+typedef struct gerbong {
+    int a;
+} gerbong;
+
+typedef struct gerobak {
+    int b;
+} gerobak;
+
+int comp(const void *c, const void *d){
+    gerbong *dataC = (gerbong *)c;
+    gerbong *dataD = (gerbong *)d;
+    return(dataC->a - dataD->a);
+}
+
+int comp2(const void *e, const void *f){
+    gerobak *dataE = (gerobak *)e;
+    gerobak *dataF = (gerobak *)f;
+    return(dataE->b - dataF->b);
+}
+
+int main (){
+    int i, N, M;
+    gerbong *kru;
+    scanf("%d", &N);
+
+    kru = (gerbong*) malloc(N * sizeof(gerbong));
+
+    for (i = 0; i < N; i++) {
+        scanf("%d", &kru[i].a);
+    }
+
+    gerobak *crew;
+    scanf("%d", &M);
+
+    crew = (gerobak*) malloc(M * sizeof(gerobak));
+
+    for (i = 0; i < M; i++) {
+        scanf("%d", &crew[i].b);
+    }
+
+    for (i = 0; i < N; i++) {
+        printf("%d ", kru[i].a);
+    }
+
+    for (i = 0; i < M; i++) {
+        printf("%d ", crew[i].b);
+    }
+
+    free(kru);
     return 0;
 }
